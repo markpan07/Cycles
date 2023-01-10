c431d6d1c2de41361fb7a23e3d1416a70bdb15b1
МАРК <markpan07@mail.ru>
2023-01-10 16:55:40 +0300

    4 task are done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        System.out.println("Задача 3");
+        for (int a = 0; a>=0 && a <= 17 && a % 2 == 0; a+=2) {
+            System.out.println(a);
+        }
+
+        System.out.println();
+        System.out.println("Задача 4");
+        for (int a = 10; a>=-10 && a <= 10; a--) {
+            System.out.println(a);
+        }
     }
 }
